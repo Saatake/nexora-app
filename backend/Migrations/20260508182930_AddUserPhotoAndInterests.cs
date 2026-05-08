e77a9b8f76310e8f91f9e444344dbeea8cd3490a
Saatake <edsonsatake510@gmail.com>
2026-05-08 15:30:21 -0300

    feat: migration para PhotoUrl e Interests

---
+++ backend/Migrations/20260508182930_AddUserPhotoAndInterests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPhotoAndInterests : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Interests",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Interests",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                table: "AspNetUsers");
+        }
+    }
+}
